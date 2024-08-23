e9aca1641e0cb09a6ee71a9855a267b0f63aac13
Daniel Leon Herrero <danielleon157@outlook.com>
2024-08-23 20:40:23 +0200

    Writting effect for roles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import { useEffect, useRef, useState } from "react";
-import Header from "./components/header/header"
+import Header from "./components/header/header";
 
 function App() {
   const [headerBg, setHeaderBg] = useState("transparent");
   const [textColor, setColorText] = useState("white");
   const sectionRef = useRef(null);
+  const [index, setIndex] = useState(0);
+  const [text, setText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const roles = ["de Aplicaciones", "Web", "de API's"];
+
+
+  useEffect(() => {
+    if (sectionRef.current) {
+      const timeout = setTimeout(() => {
+        setText((prev) => roles[index].substring(0, prev.length + (isDeleting ? -1 : 1)));
+
+        if (!isDeleting && text === roles[index]) {
+          setTimeout(() => setIsDeleting(true), 500);
+        } else if (isDeleting && text === "") {
+          setIsDeleting(false);
+          setIndex((prevIndex) => prevIndex < roles.length - 1 ? prevIndex + 1 : 0);
+        }
+
+        clearTimeout(timeout);
+      }, isDeleting ? 100 : 200); // Controla la velocidad de escritura y borrado
+
+      return () => clearTimeout(timeout);
+    }
+  }, [text, isDeleting, index]);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -26,6 +51,7 @@ function App() {
     };
   }, []);
 
+
   return (
     <>
       <Header className={`fixed top-0 left-0 right-0 z-50 text-${textColor} bg-${headerBg}`} />
@@ -35,7 +61,7 @@ function App() {
           <section className="flex flex-col gap-5 items-center justify-center text-white m-10 text-center">
             <p className="text-lg sm:text-3xl">Hola! 👋🏻</p>
             <p className="font-bold text-3xl sm:text-6xl lg:text-7xl">Soy Daniel Leon Herrero</p>
-            <p className="text-xl md:text-4xl">Un Desarrollador de </p>
+            <p className="text-xl md:text-4xl">Un Desarrollador {text}<span className="animate-blink">|</span></p>
           </section>
         </div>
       </div>
