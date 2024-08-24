f605f0f22e57d5503e4c060a1b31febc3a25422c
Daniel Leon Herrero <danielleon157@outlook.com>
2024-08-24 13:06:53 +0200

    Sections modularized

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import Header from "./components/header/header";
-import TypingEffect from "./components/typing/typing";
 import ThemeProvider from './context/themeContext/themeProvider.jsx';
+import WelcomeSection from "./components/sections/welcome/welcomeSection";
+import WorkSection from "./components/sections/works/workSection";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false)
   const [headerBg, setHeaderBg] = useState("transparent");
   const [textColor, setColorText] = useState("white");
   const sectionRef = useRef(null);
-
-  const roles = ["de Aplicaciones", "Web", "de API's"];
 
   useEffect(() => {
 
@@ -48,25 +47,8 @@ function App() {
     <>
       <ThemeProvider>
         <Header className={`fixed top-0 left-0 right-0 z-50 text-${textColor} bg-${headerBg}`} menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <div className="relative h-screen bg-center bg-cover text-white" style={{ backgroundImage: "url('/images/background.jpg')", backgroundAttachment: "fixed" }}>
-          <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="absolute inset-0 flex items-center justify-center">
-            <section className="flex flex-col gap-5 items-center justify-center text-white m-10 text-center">
-              <p className="text-lg sm:text-3xl">Hola! 👋🏻</p>
-              <p className="font-bold text-3xl sm:text-6xl lg:text-7xl">Soy Daniel Leon Herrero</p>
-              <p className="text-xl md:text-4xl">
-                <span>Un Desarrollador </span>
-                {<TypingEffect textList={roles} />}
-                <span className="animate-blink">|</span></p>
-            </section>
-          </div>
-        </div>
-        <div ref={sectionRef} className="relative h-screen bg-white text-black">
-          <section className="flex flex-col gap-5 items-center justify-center text-black m-10 text-center">
-            <p className="text-lg sm:text-3xl">Nueva Sección</p>
-            <p className="font-bold text-3xl sm:text-6xl lg:text-7xl">Contenido de la nueva sección</p>
-          </section>
-        </div>
+        <WelcomeSection/>
+        <WorkSection sectionRef={sectionRef} />
       </ThemeProvider>
     </>
   );
---
+++ src/components/sections/welcome/welcomeSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import TypingEffect from '../../typing/typing';
+
+
+const WelcomeSection = (props) => {
+    const roles = ["de Aplicaciones", "Web", "de API's"];
+    return (
+        <>
+            <div className="relative h-screen bg-center bg-cover text-white" style={{ backgroundImage: "url('/images/background.jpg')", backgroundAttachment: "fixed" }}>
+                <div className="absolute inset-0 bg-black opacity-50"></div>
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <section className="flex flex-col gap-5 items-center justify-center text-white m-10 text-center">
+                        <p className="text-lg sm:text-3xl">Hola! 👋🏻</p>
+                        <p className="font-bold text-3xl sm:text-6xl lg:text-7xl">Soy Daniel Leon Herrero</p>
+                        <p className="text-xl md:text-4xl">
+                            <span>Un Desarrollador </span>
+                            {<TypingEffect textList={roles} />}
+                            <span className="animate-blink">|</span></p>
+                    </section>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default WelcomeSection;
